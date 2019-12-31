d612f0ccfb4e1efe26989b8c48fe4ae2d93772de
Steven Bruno <sbruno636@gmail.com>
2019-12-31 13:09:55 -0600

    create list of available paths

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const fs = require('fs');
 
 http.createServer(function (req, res) {
   const q = url.parse(req.url, true);
-  console.table(q);
   const filename = q.pathname.length > 1 ? "." + q.pathname + ".html" : "./index.html";
+
+  //create list of available paths
+  const availablePaths = [];
+  fs.readdir("./", (err, files) => {
+    files.forEach(file => {
+      if (file.endsWith(".html")) {
+        availablePaths.push("./" + file);
+      }
+    })
+  });
+
+
+  //check if filename in list of available paths
+  //if true, do the fn below, if false, show 404.html
+
   fs.readFile(filename, function(err, data) {
     if (err) {
-      //display 404 page
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found, default message");
     }  
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
