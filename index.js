01af23d6073234bbd1ceae6aba821f16fe2e639c
Steven Bruno <sbruno636@gmail.com>
2019-12-31 18:17:04 -0600

    route to custom 404 page on invalid page request

--- index.js
+++ index.js
@@ -6,27 +6,29 @@ http.createServer(function (req, res) {
   const q = url.parse(req.url, true);
   const filename = q.pathname.length > 1 ? "." + q.pathname + ".html" : "./index.html";
 
-  //create list of available paths
-  const availablePaths = [];
-  fs.readdir("./", (err, files) => {
-    files.forEach(file => {
-      if (file.endsWith(".html")) {
-        availablePaths.push("./" + file);
-      }
-    })
-  });
+  const availablePaths = ["./404.html", "./about.html", "./contact-me.html", "./index.html", "/"];
 
-
-  //check if filename in list of available paths
-  //if true, do the fn below, if false, show 404.html
-
-  fs.readFile(filename, function(err, data) {
-    if (err) {
+  if (availablePaths.includes(filename)) {
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found, default message");
+      }  
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }
+  else {
+    fs.readFile("./404.html", function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found, default message");
+      }  
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found, default message");
-    }  
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+      res.write(data);
+      return res.end();
+    });
+  }
+
 }).listen(8080); 
